aae47921db7fad24cf374fd99d29766d791e9fe8
Matt Driscoll <mdriscoll@esri.com>
2012-08-27 14:11:14 -0700

    no message

--- resources/configJSON.js
+++ resources/configJSON.js
@@ -1,20 +1,360 @@
 {
    "configurationSettings":[
       {
-         "category":"<b>Content Options</b>",
+         "category":"<b>General Options</b>",
          "fields":[
             {
                "type":"string",
-               "fieldName":"group",
-               "label":"Group ID:",
-               "tooltip":"ID of the group.",
+               "fieldName":"webmap",
+               "label":"Webmap ID",
+               "tooltip":"Webmap ID",
                "placeHolder":""
+            },
+            {
+               "type":"paragraph",
+               "value":"The ID of the webmap to use for this template."
+            },
+            {
+               "type":"string",
+               "fieldName":"defaultMenu",
+               "tooltip":"Default Menu",
+               "label":"Default Menu:",
+               "options":[
+                  {
+                     "label":"Places",
+                     "value":"places"
+                  },
+                  {
+                     "label":"Basemap",
+                     "value":"basemap"
+                  },
+                  {
+                     "label":"Layers",
+                     "value":"layers"
+                  },
+                  {
+                     "label":"Social Media",
+                     "value":"social"
+                  },
+                  {
+                     "label":"Legend",
+                     "value":"legend"
+                  }
+               ]
+            },
+            {
+               "type":"paragraph",
+               "value":"The menu that is shown initially."
+            },
+            {
+               "type":"boolean",
+               "fieldName":"showSearchBox",
+               "label":"Show Search Box",
+               "tooltip":"Show Search Box"
+            },
+            {
+               "type":"boolean",
+               "fieldName":"showShareMenu",
+               "label":"Show Share Menu",
+               "tooltip":"Show Share Menu"
+            },
+            {
+               "type":"boolean",
+               "fieldName":"showSocialMenu",
+               "label":"Show Social Menu",
+               "tooltip":"Show Social Menu"
+            },
+            {
+               "type":"boolean",
+               "fieldName":"showBasemapMenu",
+               "label":"Show Basemap Menu",
+               "tooltip":"Show Basemap Menu"
+            },
+            {
+               "type":"boolean",
+               "fieldName":"showLayersMenu",
+               "label":"Show Layers Menu",
+               "tooltip":"Show Layers Menu"
+            },
+            {
+               "type":"boolean",
+               "fieldName":"showLegendMenu",
+               "label":"Show Legend Menu",
+               "tooltip":"Show Legend Menu"
+            },
+            {
+               "type":"boolean",
+               "fieldName":"showPlaces",
+               "label":"Show Places Menu",
+               "tooltip":"Show Places Menu"
+            },
+            {
+               "type":"boolean",
+               "fieldName":"showGeolocation",
+               "label":"Show Geolocate Option",
+               "tooltip":"Show Geolocate Option"
+            },
+            {
+               "type":"boolean",
+               "fieldName":"showAboutDialog",
+               "label":"Show About Dialog",
+               "tooltip":"Show About Dialog"
+            },
+            {
+               "type":"paragraph",
+               "value":"Show the about button and dialog."
+            },
+            {
+               "type":"string",
+               "fieldName":"socialDistance",
+               "tooltip":"Social Distance",
+               "label":"Social Distance:",
+               "options":[
+                  {
+                     "label":"Local",
+                     "value":"local"
+                  },
+                  {
+                     "label":"Regional",
+                     "value":"regional"
+                  },
+                  {
+                     "label":"National",
+                     "value":"national"
+                  }
+               ]
+            },
+            {
+               "type":"paragraph",
+               "value":"Distance to search for social media."
+            },
+            {
+               "type":"string",
+               "fieldName":"socialDisplay",
+               "tooltip":"Social Display",
+               "label":"Social Display:",
+               "options":[
+                  {
+                     "label":"Cluster",
+                     "value":"cluster"
+                  },
+                  {
+                     "label":"Heatmap",
+                     "value":"heatmap"
+                  }
+               ]
+            },
+            {
+               "type":"paragraph",
+               "value":"Display social media as clusters or density."
+            }
+         ]
+      },
+      {
+         "category":"<b>Twitter Options</b>",
+         "fields":[
+            {
+               "type":"boolean",
+               "fieldName":"showTwitter",
+               "label":"Show Twitter",
+               "tooltip":"Show Twitter"
+            },
+            {
+               "type":"paragraph",
+               "value":"Show Twitter on this template."
+            },
+            {
+               "type":"boolean",
+               "fieldName":"showTwitterConfig",
+               "label":"Show Config",
+               "tooltip":"Show Config"
+            },
+            {
+               "type":"paragraph",
+               "value":"Allow configuration keywords to be changed."
+            },
+            {
+               "type":"boolean",
+               "fieldName":"twitterChecked",
+               "label":"Checked",
+               "tooltip":"Checked"
+            },
+            {
+               "type":"paragraph",
+               "value":"Turn this layer on by default."
+            },
+            {
+               "type":"string",
+               "fieldName":"twitterSearch",
+               "label":"Search Keywords",
+               "tooltip":"Search Keywords",
+               "placeHolder":""
+            }
+         ]
+      },
+      {
+         "category":"<b>Flickr Options</b>",
+         "fields":[
+            {
+               "type":"boolean",
+               "fieldName":"showFlickr",
+               "label":"Show Flickr",
+               "tooltip":"Show Flickr"
+            },
+            {
+               "type":"paragraph",
+               "value":"Show Flickr on this template."
+            },
+            {
+               "type":"boolean",
+               "fieldName":"showFlickrConfig",
+               "label":"Show Config",
+               "tooltip":"Show Config"
+            },
+            {
+               "type":"paragraph",
+               "value":"Allow configuration keywords to be changed."
+            },
+            {
+               "type":"boolean",
+               "fieldName":"flickrChecked",
+               "label":"Checked",
+               "tooltip":"Checked"
+            },
+            {
+               "type":"paragraph",
+               "value":"Turn this layer on by default."
+            },
+            {
+               "type":"string",
+               "fieldName":"flickrSearch",
+               "label":"Search Keywords",
+               "tooltip":"Search Keywords",
+               "placeHolder":""
+            },
+            {
+               "type":"string",
+               "fieldName":"flickrRange",
+               "tooltip":"Date Range",
+               "label":"Date Range:",
+               "options":[
+                  {
+                     "label":"today",
+                     "value":"today"
+                  },
+                  {
+                     "label":"this_week",
+                     "value":"this_week"
+                  },
+                  {
+                     "label":"this_month",
+                     "value":"this_month"
+                  },
+                  {
+                     "label":"all_time",
+                     "value":"all_time"
+                  }
+               ]
+            }
+         ]
+      },
+      {
+         "category":"<b>YouTube Options</b>",
+         "fields":[
+            {
+               "type":"boolean",
+               "fieldName":"showYouTube",
+               "label":"Show YouTube",
+               "tooltip":"Show YouTube"
+            },
+            {
+               "type":"paragraph",
+               "value":"Show YouTube on this template."
+            },
+            {
+               "type":"boolean",
+               "fieldName":"showYouTubeConfig",
+               "label":"Show Config",
+               "tooltip":"Show Config"
+            },
+            {
+               "type":"paragraph",
+               "value":"Allow configuration keywords to be changed."
+            },
+            {
+               "type":"boolean",
+               "fieldName":"youtubeChecked",
+               "label":"Checked",
+               "tooltip":"Checked"
+            },
+            {
+               "type":"paragraph",
+               "value":"Turn this layer on by default."
+            },
+            {
+               "type":"string",
+               "fieldName":"youtubeSearch",
+               "label":"Search Keywords",
+               "tooltip":"Search Keywords",
+               "placeHolder":""
+            },
+            {
+               "type":"paragraph",
+               "value":"<a href=\"http:\/\/support.twitter.com\/articles\/71577-how-to-use-advanced-twitter-search\" target=\"_blank\">Advanced info<\/a>."
+            },
+            {
+               "type":"string",
+               "fieldName":"youtubeRange",
+               "tooltip":"Date Range",
+               "label":"Date Range:",
+               "options":[
+                  {
+                     "label":"today",
+                     "value":"today"
+                  },
+                  {
+                     "label":"this_week",
+                     "value":"this_week"
+                  },
+                  {
+                     "label":"this_month",
+                     "value":"this_month"
+                  },
+                  {
+                     "label":"all_time",
+                     "value":"all_time"
+                  }
+               ]
             }
          ]
       }
    ],
    "values":{
-      "theme":"blueTheme",
-      
+      "defaultMenu":"social",
+      "showSearchBox":true,
+      "showShareMenu":true,
+      "showSocialMenu":true,
+      "showBasemapMenu":true,
+      "showLayersMenu":true,
+      "showLegendMenu":true,
+      "showPlaces":true,
+      "showGeolocation":true,
+      "showAboutDialog":true,
+      "socialDistance":"regional",
+      "socialDisplay":"cluster",
+      "showFlickr":true,
+      "showFlickrConfig":true,
+      "flickrChecked":false,
+      "flickrSearch":"puppies",
+      "flickrRange":"this_week",
+      "showYouTube":true,
+      "showYouTubeConfig":true,
+      "youtubeChecked":true,
+      "youtubeSearch":"puppies",
+      "youtubeRange":"this_month",
+      "showTwitter":true,
+      "showTwitterConfig":true,
+      "twitterChecked":false,
+      "twitterSearch":"#puppies"
    }
 }
