2aa5e1c460b8904c9764d1124cfb10a7d5c859f8
Matt Driscoll <mdriscoll@esri.com>
2013-03-07 15:23:15 -0800

    fix redirect and cancel.

--- resources/twitter_oauth/callback.php
+++ resources/twitter_oauth/callback.php
@@ -5,29 +5,32 @@ session_start();
 require_once('twitteroauth/twitteroauth.php');
 require_once('config.php');
 
-// Create TwitteroAuth object with app key/secret and token key/secret from default phase
-$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $_SESSION['oauth_token'], $_SESSION['oauth_token_secret']);
-
-// if verifier set
-if(isset($_REQUEST['oauth_verifier'])){
-    // get access token from twitter
-    $access_token = $connection->getAccessToken($_REQUEST['oauth_verifier']);
-    // save token
-    $_SESSION['oauth_access_token'] = $access_token;
-    // 1 year
-    $cookie_life = time() + 31536000;
-    // set cookie
-    setcookie(OAUTH_COOKIE, json_encode($access_token), $cookie_life, '/', OAUTH_COOKIE_DOMAIN);
-}
-
+// check if cookie exists
 if(isset($_COOKIE[OAUTH_COOKIE])){
-    // redirect
+    // redirect back to app
     if(isset($_SESSION['oauth_referrer'])){
         header('Location: '.$_SESSION['oauth_referrer']);
-    } else {
-        header('Location: ./index.php');
     }
 }
 else{
-    header('Location: ./callback.php');
+    // if verifier set
+    if(isset($_REQUEST['oauth_verifier'])){
+        // Create TwitteroAuth object with app key/secret and token key/secret from default phase
+        $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $_SESSION['oauth_token'], $_SESSION['oauth_token_secret']);
+        // get access token from twitter
+        $access_token = $connection->getAccessToken($_REQUEST['oauth_verifier']);
+        // save token
+        $_SESSION['oauth_access_token'] = $access_token;
+        // 1 year
+        $cookie_life = time() + 31536000;
+        // set cookie
+        setcookie(OAUTH_COOKIE, json_encode($access_token), $cookie_life, '/', OAUTH_COOKIE_DOMAIN);
+        header('Location: ./callback.php');
+    }
+    else{
+       // redirect
+        if(isset($_SESSION['oauth_referrer'])){
+            header('Location: '.$_SESSION['oauth_referrer']);
+        }
+    }
 }
--- resources/twitter_oauth/switch_account.php
+++ resources/twitter_oauth/switch_account.php
@@ -2,38 +2,44 @@
 
 // Load required lib files.
 session_start();
-session_destroy();
-session_start();
 require_once('twitteroauth/twitteroauth.php');
 require_once('config.php');
 header('Content-Type: application/json');
 
-// connect to twitter
-$auth_connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET);
-
-// Get temporary credentials.
-$request_token = $auth_connection->getRequestToken(OAUTH_CALLBACK);
-
-// Save temporary credentials to session.
-$_SESSION['oauth_token'] = $request_token['oauth_token'];
-$_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
-
-if(isset($_SERVER['HTTP_REFERER'])){
-    $_SESSION['oauth_referrer'] = $_SERVER['HTTP_REFERER'];
+// check if cookie exists
+if(isset($_COOKIE[OAUTH_COOKIE])){
+    // remove session
+    session_destroy();
+    session_start();
+    // set referrer url to return to
+    if(isset($_SERVER['HTTP_REFERER']) && !isset($_SESSION['oauth_referrer'])){
+        $_SESSION['oauth_referrer'] = $_SERVER['HTTP_REFERER'];
+    }
+    // remove cookie
+    setcookie(OAUTH_COOKIE, json_encode($access_token), time() - 3600, '/', OAUTH_COOKIE_DOMAIN);
+    // reload
+    header('Location: ./switch_account.php');
 }
-
-// If last connection failed don't display authorization link.
-switch ($auth_connection->http_code) {
-    case 200:
-        // Build authorize URL and redirect user to Twitter
-        $url = $auth_connection->getAuthorizeURL($request_token['oauth_token'], false).'&force_login=true';
-        break;
-    default:
-        // error code
-        $url = $_SESSION['oauth_referrer'];
-}
-
-// redirect
-if(isset($url)){
-    header('Location: '.$url);
+else{
+    // connect to twitter
+    $auth_connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET);
+    // Get temporary credentials.
+    $request_token = $auth_connection->getRequestToken(OAUTH_CALLBACK);
+    // Save temporary credentials to session.
+    $_SESSION['oauth_token'] = $request_token['oauth_token'];
+    $_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
+    // If last connection failed don't display authorization link.
+    switch ($auth_connection->http_code) {
+        case 200:
+            // Build authorize URL and redirect user to Twitter
+            $url = $auth_connection->getAuthorizeURL($request_token['oauth_token'], false).'&force_login=true';
+            break;
+        default:
+            // error code
+            $url = $_SESSION['oauth_referrer'];
+    }
+    // redirect
+    if(isset($url)){
+        header('Location: '.$url);
+    }
 }
