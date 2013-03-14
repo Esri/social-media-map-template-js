f19cf629c060d09c4162b497d73001267d0f8722
Matt Driscoll <mdriscoll@esri.com>
2013-03-14 11:41:37 -0700

    no message

--- resources/twitter_oauth/config.php
+++ resources/twitter_oauth/config.php
@@ -5,8 +5,8 @@
  * A single location to store configuration.
  */
 
-define('CONSUMER_KEY', 'bqNeQgeAD7YVJmbRHE3VQ');
-define('CONSUMER_SECRET', 'MkZxdRjpexHC3CGdf4nOklsMimNkyoQvk9P60xEp1fY');
+define('CONSUMER_KEY', 'nJwI9WNWERSVeltecmTQ');
+define('CONSUMER_SECRET', 'HOt29mDCIJ32V0mwGM3WGpaueZsr6O9crz78YjevALY');
 define('OAUTH_CALLBACK', 'http://webninja.esri.com/git/social-media-map-template-js/resources/twitter_oauth/callback.php');
 define('OAUTH_COOKIE', 'template_twitter_auth');
 define('OAUTH_COOKIE_DOMAIN', '.esri.com');
