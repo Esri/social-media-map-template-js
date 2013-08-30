e1b96dec354acb45c6945e4ece47b4d5d205ff76
Matt Driscoll <mdriscoll@esri.com>
2013-08-30 15:40:37 -0700

    Merge fixes

---
+++ SocialMedia/nls/et/template.js
@@ -0,0 +1,163 @@
+﻿define(
+({
+        "viewer": {
+        	"main": {
+                "scaleBarUnits": "inglise",
+                "timePattern": "h:mma", // added 2.5.2013
+                "datePattern": "MMM d, yyyy" // added 2.5.2013
+            },
+			"errors": {
+                "createMap": "Kaarti ei saa luua",
+                "general": "Viga",
+                "bingError": "Selle rakenduse rakendamiseks on vajalik Teie oma Bing Maps võti.",
+                "noLegend": "Legend puudub.",
+                "heatmap": "Intensiivsuskaart ei ole selles brauseris toetatud.",
+                "noText": "Palun sisesta otsingu asukoht.",
+                "noLocation": "Asukohta ei leitud.",
+                "integersOnly": "Sellele väljale võid sisestada ainult täisarve.",
+				"nodesc": "Kirjeldus puudub."
+            },
+			"legend": {
+				"menuTitle": "Legend"
+			},
+            "search": {
+            	"location": "Asukoht",
+            	"clearLocation": "Tühista asukoht",
+            	"placeholder": "Leia koht"
+            },
+			"layers": {
+				"menuTitle": "Toimivad kihid"
+			},
+            "layer": {
+            	"information": "Info",
+            	"transparency": "Läbipaistvus",
+            	"searchSettings": "Otsingu seaded",
+            	"filteredBy": "filtreeritud:"
+            },
+            "general": {
+            	"at": ":",
+				"of": "/",
+            	"homeExtent": "Lae avavaade",
+				"ok": "OK",
+				"close": "Sulge"
+            },
+			"basemap": {
+				"menuTitle": "Vali aluskaart"
+			},
+            "settings": {
+            	"title": "Seaded",
+            	"searchAll": "Otsi kõiki",
+            	"usingThisKeyword": "Kasutades otsingusõnu",
+            	"search": "Otsi",
+            	"fromThePast": "Varasemaid",
+            	"today": "Päev",
+            	"this_week": "Nädal",
+            	"this_month": "Kuu",
+            	"all_time": "Kogu aeg",
+            	"atLocation": "Selles asukohas",
+            	"centerOfMap": "Kaardi keskpunkt",
+            	"centerOfMapTitle": "Kasuta kaardi keskpunkti",
+            	"withinThisDistance": "Kuni kaugusel",
+            	"latitude": "Laius:",
+            	"longitude": "Pikkus:",
+            	"locationText": "alguspunkti määramiseks kliki kaardil",
+            	"twSearch": "Kuidas kasutada täiustatud Twitteri otsingut"
+            },
+			"autoComplete": {
+				"menuTitle": "Tulemused&hellip;"
+			},
+            "places": {
+				"menuTitle": "Järjehoidjaga kohad",
+            	"places": "Järjehoidjad",
+            	"placesTitle": "Järjehoidja kohad",
+            	"myLocation": "Minu praegune asukoht",
+            	"myLocationTitle": "Tsentreeri kaart minu asukohta"
+            },
+            "distanceSlider": {
+            	"local": "Kohalik",
+            	"regional": "Piirkondlik",
+            	"national": "Rahvuslik"
+            },
+            "about": {
+            	"title": "Info",
+            	"access": "Ligipääsu ja kasutamise piirangud"
+            },
+            "buttons": {
+            	"legend": "Legend",
+            	"legendTitle": "Näita legendi",
+            	"basemap": "Aluskaart",
+            	"basemapTitle": "Vaheta aluskaart",
+            	"layers": "Kihid",
+            	"layersTitle": "Tutvu kihtidega",
+            	"social": "Sotsiaalne",
+            	"socialTitle": "Sotsiaalmeedia",
+            	"link": "Link",
+            	"linkTitle": "Jaga seda veebirakendust",
+            	"about": "Info",
+            	"aboutTitle": "Selle kaardi kohta",
+            	"displayAs": "Kuva kui",
+            	"point": "Punktid",
+            	"cluster": "Klastrid",
+            	"heatmap": "Tihedus"
+            },
+            "shareMenu": {
+				"menuTitle": "Jaga praegust vaadet",
+				"shareHeader": "Jaga oma veebirakenduse linki",
+            	"facebook": "Facebook",
+            	"facebookHeader": "Jaga Facebookis",
+            	"twitter": "Twitter",
+            	"twitterHeader": "Jaga Twitteris",
+            	"instructionHeader": "Kopperi/kleebi HTML oma veebilehele",
+            	"preview": "Eelvaata ja kohanda"
+            },
+            "itemInfo": {
+				"createdLabel": "loodud",
+				"ratingsLabel": "hinnang",
+				"ratingsLabelPlural": "hinnangut",
+				"viewsLabel": "vaatamine",
+				"viewsLabelPlural": "vaatamisi",
+				"commentsLabel": "kommentaar",
+				"commentsLabelPlural": "kommentaari",
+				"modifiedLabel": "Viimati muudetud",
+				"by": "autor:",
+				"separator": ","
+			},
+			"social": {
+				"menuTitle": "Sotsiaalmeedia kihid"
+			},
+			"preview": {
+				"minWidth": "Minimaalne laius on",
+				"minHeight": "Minimaalne kõrgus on",
+				"maxWidth": "Maksimaalne laius on",
+				"maxHeight": "Maksimaalne kõrgus on",
+				"customize": "Kohanda",
+				"small": "Väike",
+				"medium": "Keskmine",
+				"large": "Suur",
+				"custom": "Kohandatud",
+				"embed": "Lisa",
+				"instruction": "Kopeeri ja kleebi järgmine HTML, et lisada kaart oma veebilehele."
+			},
+			"flickr": {
+				"title": "Flickr",
+				"description": "Fotod Flickr keskkonnast"
+			},
+			"twitter": {
+				"title": "Twitter",
+				"description": "Säutsud Twitterist"
+			},
+			"youtube": {
+				"title": "YouTube",
+				"description": "Videod YouTube-st"
+			},
+			"panoramio":{
+				"title": "Panoramio",
+				"description": "Fotod Panoramio-st"
+			},
+			"ushahidi":{
+				"title": "Ushahidi",
+				"description": "Juhtumite aruanded Ushahidi-st"
+			}
+        }
+    })
+);
---
+++ SocialMedia/nls/lv/template.js
@@ -0,0 +1,163 @@
+﻿define(
+({
+        "viewer": {
+        	"main": {
+                "scaleBarUnits": "metric",
+                "timePattern": "h:mma", // added 2.5.2013
+                "datePattern": "MMM d, yyyy" // added 2.5.2013
+            },
+			"errors": {
+                "createMap": "Nevar izveidot karti",
+                "general": "Kļūda",
+                "bingError": "Šīs aplikācijas izvietošana prasa jūsu pašu Bing Maps taustiņu.",
+                "noLegend": "Nav leģendas.",
+                "heatmap": "Siltumkartēšana nav atbalstīta ar šo pārlūku.",
+                "noText": "Lūdzu ievadiet meklēšanas izvietojumu.",
+                "noLocation": "Izvietojumu nevar atrast.",
+                "integersOnly": "Šajā laukā jūs varat ievadīt tikai veselus skaitļus.",
+				"nodesc": "Nav apraksta."
+            },
+			"legend": {
+				"menuTitle": "Leģenda"
+			},
+            "search": {
+            	"location": "Izvietojums",
+            	"clearLocation": "Notīrīt izvietojumu",
+            	"placeholder": "Atrast vietu"
+            },
+			"layers": {
+				"menuTitle": "Darbības slāņi"
+			},
+            "layer": {
+            	"information": "Informācija",
+            	"transparency": "Caurspīdīgums",
+            	"searchSettings": "Meklēšanas iestatījumi",
+            	"filteredBy": "filtrēts pēc:"
+            },
+            "general": {
+            	"at": "uz",
+				"of": "no",
+            	"homeExtent": "Ielādē mājas skatu",
+				"ok": "Labi",
+				"close": "Aizvērt"
+            },
+			"basemap": {
+				"menuTitle": "Izvēlieties pamatkarti"
+			},
+            "settings": {
+            	"title": "Iestatījumi",
+            	"searchAll": "Meklē visus",
+            	"usingThisKeyword": "Izmantojot atslēgvārdu(s)",
+            	"search": "Meklēt",
+            	"fromThePast": "No pagātnes",
+            	"today": "Diena",
+            	"this_week": "Nedēļa",
+            	"this_month": "Mēnesis",
+            	"all_time": "Visu laiku",
+            	"atLocation": "Šinī izvietojumā",
+            	"centerOfMap": "Kartes centrs",
+            	"centerOfMapTitle": "Lietojiet kartes centru",
+            	"withinThisDistance": "Šajā attālumā",
+            	"latitude": "Plat:",
+            	"longitude": "Gar:",
+            	"locationText": "noklikšķiniet uz kartes, lai iestatītu koordinātu sākuma punktu",
+            	"twSearch": "Kā lietot uzlaboto Twitter meklētāju"
+            },
+			"autoComplete": {
+				"menuTitle": "Resultāti&hellip;"
+			},
+            "places": {
+				"menuTitle": "Grāmatzīmju vietas",
+            	"places": "Grāmatzīmes",
+            	"placesTitle": "Grāmatzīmes vietas",
+            	"myLocation": "Mans pašreizējais izvietojums",
+            	"myLocationTitle": "Centrēt karti uz manu izvietojumu"
+            },
+            "distanceSlider": {
+            	"local": "Lokāls",
+            	"regional": "Reģionāls",
+            	"national": "Nacionāls"
+            },
+            "about": {
+            	"title": "Par",
+            	"access": "Piekļuves un izmantošanas ierobežojumi"
+            },
+            "buttons": {
+            	"legend": "Leģenda",
+            	"legendTitle": "Rādīt leģendu",
+            	"basemap": "Pamatkarte",
+            	"basemapTitle": "Pārslēgt pamatkarti",
+            	"layers": "Slāņi",
+            	"layersTitle": "Izpētīt slāņus",
+            	"social": "Sabiedrisks",
+            	"socialTitle": "Sabiedrisks medijs",
+            	"link": "Saite",
+            	"linkTitle": "Koplietot šo web lietotni",
+            	"about": "Par",
+            	"aboutTitle": "Par šo karti",
+            	"displayAs": "Parādīt kā",
+            	"point": "Punkti",
+            	"cluster": "Klasteri",
+            	"heatmap": "Blīvums"
+            },
+            "shareMenu": {
+				"menuTitle": "Koplietot pašreizējo skatu",
+				"shareHeader": "Koplietot saiti uz jūsu web lietotni",
+            	"facebook": "Facebook",
+            	"facebookHeader": "Koplietot Facebook",
+            	"twitter": "Twitter",
+            	"twitterHeader": "Koplietot Twitter",
+            	"instructionHeader": "Kopēt/ielīmēt HTML jūsu web vietnē",
+            	"preview": "Priekšskatīt un pielāgot"
+            },
+            "itemInfo": {
+				"createdLabel": "izveidots",
+				"ratingsLabel": "novērtējums",
+				"ratingsLabelPlural": "novērtējumi",
+				"viewsLabel": "skats",
+				"viewsLabelPlural": "skati",
+				"commentsLabel": "komentārs",
+				"commentsLabelPlural": "komentāri",
+				"modifiedLabel": "Pēdējā modificēšana",
+				"by": "ar",
+				"separator": ","
+			},
+			"social": {
+				"menuTitle": "Sabiedriska medija slāņi"
+			},
+			"preview": {
+				"minWidth": "Minimālais platums ir",
+				"minHeight": "Minimālais augstums ir",
+				"maxWidth": "Maksimālais platums ir",
+				"maxHeight": "Maksimālais augstums ir",
+				"customize": "Pielāgot",
+				"small": "Mazs",
+				"medium": "Vidējs",
+				"large": "Liels",
+				"custom": "Pielāgots",
+				"embed": "Iedarina",
+				"instruction": "Kopēt un ielīmēt sekojošo HTML, lai iedarinātu karti jūsu web vietnē."
+			},
+			"flickr": {
+				"title": "Flickr",
+				"description": "Foto no Flickr"
+			},
+			"twitter": {
+				"title": "Twitter",
+				"description": "Tvīti no Tvitera"
+			},
+			"youtube": {
+				"title": "YouTube",
+				"description": "Video no YouTube"
+			},
+			"panoramio":{
+				"title": "Panorāma",
+				"description": "Foto no panorāmas"
+			},
+			"ushahidi":{
+				"title": "Ushahidi",
+				"description": "Incidentu ziņojumi no Ushahidi"
+			}
+        }
+    })
+);
